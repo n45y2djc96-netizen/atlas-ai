4c1d1deafbbfe1012f2cd8b715dc9d088e4d68a4
n45y2djc96-netizen <n45y2djc96@privaterelay.appleid.com>
2026-06-20 01:33:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = 8747579183:AAGEdi5ramP3XZ0EEzAQVOCB4IRnqvm8ANc
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 Привет!\n\n"
+        "Я ATLAS — твой личный AI-стратег.\n\n"
+        "Для начала скажи, как тебя зовут?"
+    )
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
 print("ATLAS запущен")
+app.run_polling()
