73d4db25657a4c38ebd2cb4fbefcf845ffec4fc8
n45y2djc96-netizen <n45y2djc96@privaterelay.appleid.com>
2026-07-19 11:50:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ from stars import buy_pro
 from memory import update_memory
 from atlas_memory import analyze_user
 from atlas_observer import observe_user
+from atlas_core import build_strategy
 
 TOKEN = "8747579183:AAGlnU03s7XUeFNVe3jqAgPH-UB1GiHcDJU"
 DATA_FILE = "users.json"
