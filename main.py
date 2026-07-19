461327d138a97dac437b63a6d88a9495eec85a91
n45y2djc96-netizen <n45y2djc96@privaterelay.appleid.com>
2026-07-19 18:51:39 +0600

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ from atlas_memory import analyze_user
 from atlas_observer import observe_user
 from atlas_core import build_strategy
 from atlas_brain import build_brain
-from atlas_coach import send_coach_message
 
 TOKEN = "8747579183:AAGlnU03s7XUeFNVe3jqAgPH-UB1GiHcDJU"
 DATA_FILE = "users.json"
