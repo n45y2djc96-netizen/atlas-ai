f1f75a52e69a22e08f4c885f723f83b0f62133a6
n45y2djc96-netizen <n45y2djc96@privaterelay.appleid.com>
2026-08-30 14:46:15 +0600

    Update ai.py

--- ai.py
+++ ai.py
@@ -282,7 +282,7 @@ def chat_ai(text, memory, user):
             })
         
         response = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
+            model="llama-3.3-70b-versatile",
             messages=messages,
             temperature=0.5,
             max_tokens=2048
