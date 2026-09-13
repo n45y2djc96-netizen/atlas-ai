8b0c76b6cb1cee2e75f01503d1e122ff1e20fdc9
n45y2djc96-netizen <n45y2djc96@privaterelay.appleid.com>
2026-09-14 01:51:33 +0600

    Update atlas_promises.py

--- atlas_promises.py
+++ atlas_promises.py
@@ -2,19 +2,20 @@ def remember_promise(user, text):
     t = text.lower()
 
     triggers = [
-        "завтра",
-        "начну",
-        "сделаю",
         "обещаю",
-        "буду",
-        "планирую",
-        "хочу начать"
+        "завтра точно сделаю",
+        "завтра сделаю",
+        "точно сделаю",
+        "сделаю это завтра",
+        "начну завтра"
     ]
 
-    if any(i in t for i in triggers):
+    if any(word in t for word in triggers):
 
         if "promises" not in user:
             user["promises"] = []
 
-        user["promises"].append(text)
+        if text not in user["promises"]:
+            user["promises"].append(text)
+
         user["promises"] = user["promises"][-30:]
