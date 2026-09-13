b568956636eaa6fbf690e71933a0d865313b7e2c
n45y2djc96-netizen <n45y2djc96@privaterelay.appleid.com>
2026-09-14 01:51:09 +0600

    Update atlas_personality.py

--- atlas_personality.py
+++ atlas_personality.py
@@ -4,16 +4,21 @@ def remember_personality(user, text):
     if "personality" not in user:
         user["personality"] = []
 
-    # Личные темы, которые стоит запоминать
     personal_words = [
-        "мечтаю", "люблю", "горжусь", "боюсь",
-        "хочу", "нравится", "важно", "счастлив",
-        "семья", "друзья", "детство", "будущее"
+        "мечтаю",
+        "люблю",
+        "горжусь",
+        "боюсь",
+        "мне нравится",
+        "для меня важно",
+        "моя мечта",
+        "я ненавижу",
+        "мне нравится",
+        "не люблю"
     ]
 
     if any(word in t for word in personal_words):
         if text not in user["personality"]:
             user["personality"].append(text)
 
-    # Храним только последние 30 личных ответов
     user["personality"] = user["personality"][-30:]
